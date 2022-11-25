bb6504ad3934e3846d9a3c1650abea9288b42782
bub3690 <bub3690@naver.com>
2022-11-25 15:18:02 +0900

    AUX: Update num_workers

--- model/Module/sweep/melspectro_test.sh
+++ model/Module/sweep/melspectro_test.sh
@@ -1,5 +1,5 @@
-python melspectrogram_sweep.py --seed 1004
-python melspectrogram_sweep.py --seed 1005
-python melspectrogram_sweep.py --seed 1006
-python melspectrogram_sweep.py --seed 1007
-python melspectrogram_sweep.py --seed 1008
+python melspectrogram_sweep.py --seed 1004 --num_workers 20
+python melspectrogram_sweep.py --seed 1005 --num_workers 20
+python melspectrogram_sweep.py --seed 1006 --num_workers 20
+python melspectrogram_sweep.py --seed 1007 --num_workers 20
+python melspectrogram_sweep.py --seed 1008 --num_workers 20
