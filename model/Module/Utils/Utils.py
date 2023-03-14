4961a192b1dfe012ed176f8fae28c0c160edc06b
bub3690 <bub3690@naver.com>
2023-03-15 01:16:32 +0900

    AUX: Fix path error

--- model/Module/Utils/Utils.py
+++ model/Module/Utils/Utils.py
@@ -169,6 +169,6 @@ def save_result(all_filename, all_prediction, all_answers,all_probs,speaker_file
     merge_left['result']=merge_left['prediction']==merge_left['answer']
     merge_left['filename']=merge_left['filename'].values.astype(int)
     merge_left = merge_left[['filename','fold','AGE','DETAIL','prediction','answer','prob','result']]
-    excel_name = 'D:/project/voice_pathology_ai/voice_data/results/'+args.model+'_'+args.dataset+'_seed_'+str(args.seed)+'_organics_speaker.xlsx'
+    excel_name = '../../../voice_data/results/'+args.model+'_'+args.dataset+'_seed_'+str(args.seed)+'_organics_speaker.xlsx'
     merge_left.to_excel(excel_name,index=False)
 
