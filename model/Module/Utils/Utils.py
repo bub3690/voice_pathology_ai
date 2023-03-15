0be22cfaf47aaec485006860df1ce526fb41c092
bub3690 <bub3690@naver.com>
2023-03-15 10:10:52 +0900

    AUX:Update linux utils

--- model/Module/Utils/Utils.py
+++ model/Module/Utils/Utils.py
@@ -170,5 +170,7 @@ def save_result(all_filename, all_prediction, all_answers,all_probs,speaker_file
     merge_left['filename']=merge_left['filename'].values.astype(int)
     merge_left = merge_left[['filename','fold','AGE','DETAIL','prediction','answer','prob','result']]
     excel_name = '../../../voice_data/results/'+args.model+'_'+args.dataset+'_seed_'+str(args.seed)+'_organics_speaker.xlsx'
+    print(excel_name)
+    excel_name = './'+args.model+'_'+args.dataset+'_seed_'+str(args.seed)+'_organics_speaker.xlsx'
     merge_left.to_excel(excel_name,index=False)
 
