86432c9898b44024b26a219b718ccb9a2745c006
bub3690 <bub3690@naver.com>
2023-03-21 15:20:53 +0900

    AUX: Change shell

---
+++ model/Module/shells/ablations/resnet18_ablation.ps1
@@ -0,0 +1,30 @@
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set100 --seed 1004 --data-probs 0 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set100 --seed 1005 --data-probs 0 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set100 --seed 1006 --data-probs 0 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set100 --seed 1007 --data-probs 0 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set100 --seed 1008 --data-probs 0 --save-result True --descript "wav phrase resnet18 speaker independent"
+
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set20 --seed 1004 --data-probs 1 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set20 --seed 1005 --data-probs 1 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set20 --seed 1006 --data-probs 1 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set20 --seed 1007 --data-probs 1 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set20 --seed 1008 --data-probs 1 --save-result True --descript "wav phrase resnet18 speaker independent"
+
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set40 --seed 1004 --data-probs 2 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set40 --seed 1005 --data-probs 2 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set40 --seed 1006 --data-probs 2 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set40 --seed 1007 --data-probs 2 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set40 --seed 1008 --data-probs 2 --save-result True --descript "wav phrase resnet18 speaker independent"
+
+
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set60 --seed 1004 --data-probs 3 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set60 --seed 1005 --data-probs 3 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set60 --seed 1006 --data-probs 3 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set60 --seed 1007 --data-probs 3 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set60 --seed 1008 --data-probs 3 --save-result True --descript "wav phrase resnet18 speaker independent"
+
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set80 --seed 1004 --data-probs 4 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set80 --seed 1005 --data-probs 4 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set80 --seed 1006 --data-probs 4 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set80 --seed 1007 --data-probs 4 --save-result True --descript "wav phrase resnet18 speaker independent"
+python train.py --wandb True --batch-size 16 --model wav_res --dataset phrase --tag wav_res wav phrase set80 --seed 1008 --data-probs 4 --save-result True --descript "wav phrase resnet18 speaker independent"
