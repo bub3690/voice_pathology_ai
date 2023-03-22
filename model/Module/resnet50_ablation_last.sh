1c45dab11360af6305118223861218a07f76227a
bub3690 <bub3690@naver.com>
2023-03-23 00:42:26 +0900

    FEAT: Update figures

--- model/Module/resnet50_ablation_last.sh
+++ model/Module/resnet50_ablation_last.sh
@@ -1,4 +1,4 @@
-python train.py --wandb True --batch-size 16 --model resnet50 --dataset phrase --tag resnet50 wav phrase set60 --seed 1008 --data-probs 3 --save-result True --descript "wav phrase resnet50 speaker independent"
+python train.py --wandb True --batch-size 16 --model resnet50 --dataset phrase --tag resnet50 wav phrase set40 --seed 1008 --data-probs 2 --save-result True --descript "wav phrase resnet50 speaker independent"
 python train.py --wandb True --batch-size 16 --model resnet50 --dataset phrase --tag resnet50 wav phrase set80 --seed 1005 --data-probs 4 --save-result True --descript "wav phrase resnet50 speaker independent"
 python train.py --wandb True --batch-size 16 --model resnet50 --dataset phrase --tag resnet50 wav phrase set80 --seed 1006 --data-probs 4 --save-result True --descript "wav phrase resnet50 speaker independent"
 python train.py --wandb True --batch-size 16 --model resnet50 --dataset phrase --tag resnet50 wav phrase set80 --seed 1007 --data-probs 4 --save-result True --descript "wav phrase resnet50 speaker independent"
